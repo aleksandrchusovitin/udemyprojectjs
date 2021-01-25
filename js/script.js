ff12a5f4c741dde4cd8e9c9c3f64ffca50f3908a
AlexandrChusovitin <chusovitinalexandr@gmail.com>
2021-01-25 22:57:24 +0700

    practice1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 "use strict";
 
-alert("Hello");
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+const lastWatchedMovie1 = prompt('Один из последних просмотренных фильмов', '');
+const filmRating1 = prompt('На сколько оцените его?', '');
+
+const lastWatchedMovie2 = prompt('Один из последних просмотренных фильмов', '');
+const filmRating2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastWatchedMovie1] = filmRating1;
+personalMovieDB.movies[lastWatchedMovie2] = filmRating2;
+
+console.log(personalMovieDB);
